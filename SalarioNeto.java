42680ae6ce5e982054b87b11e67a5cc3d8bdd38a
MariaGargoles <84070258+MariaGargoles@users.noreply.github.com>
2023-10-29 11:42:51 +0100

    Add files via upload

---
+++ SalarioNeto.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SalarioNeto {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el salario bruto: ");
+        double SalarioBruto = sc.nextDouble();
+
+        if (SalarioBruto < 0) {
+            System.out.println("El salario no puede ser negativo");
+            sc.close();
+            return;
+        }
+
+        System.out.println("Numero de hijos: ");
+        int NumeroHijos = sc.nextInt();
+
+        if (NumeroHijos < 0) {
+            System.out.println("No puede ser negativo");
+            sc.close();
+            return;
+        }
+
+        double retencion = 0.0;
+
+        if (NumeroHijos <= 2) {
+            retencion = SalarioBruto * 0.20;
+        } else if (NumeroHijos > 2 && NumeroHijos <= 5) {
+            retencion = SalarioBruto * 0.15;
+        } else if (NumeroHijos > 5 && NumeroHijos <= 7) {
+            retencion = SalarioBruto * 0.10;
+        } else if (NumeroHijos >= 7) {
+            retencion = SalarioBruto * 0.05;
+        }
+
+        double SalarioNeto = SalarioBruto - retencion;
+
+        System.out.println("Salario bruto: " + SalarioBruto);
+        System.out.println("Retencion: " + retencion);
+        System.out.println("Salario neto: " + SalarioNeto);
+
+        sc.close();
+    }
+}
