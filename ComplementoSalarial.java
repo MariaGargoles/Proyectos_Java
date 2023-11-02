2d1d771dc1427772bd8eca7dd8141ebf9fb58c2c
MariaGargoles <84070258+MariaGargoles@users.noreply.github.com>
2023-11-02 20:04:09 +0100

    Update ComplementoSalarial.java

--- ComplementoSalarial.java
+++ ComplementoSalarial.java
@@ -3,41 +3,49 @@ import java.util.Scanner;
 public class ComplementoSalarial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String continuar = "si";
 
-        System.out.println("Ingrese la letra correspondiente al departamento (A, B, C o D): ");
-        String departamento = scanner.nextLine().toUpperCase();
+        while (continuar.equals("si")) {
+            System.out.println("Ingrese el nombre del empleado: ");
+            String nombre = scanner.nextLine();
 
-        System.out.println("Ingrese el nombre del empleado: ");
-        String nombre = scanner.nextLine();
+            System.out.println("Ingrese el salario base del empleado: ");
+            double salarioBase = scanner.nextDouble();
+            scanner.nextLine(); // Consumir la nueva línea
 
-        System.out.println("Ingrese el salario base del empleado: ");
-        double salarioBase = scanner.nextDouble();
+            double complementoSalario = 0.0;
 
-        double complementoSalario = 0.0;
+            System.out.println("Ingrese la letra correspondiente al departamento (A, B, C o D): ");
+            String departamento = scanner.nextLine().toUpperCase();
 
-        switch (departamento) {
-            case "A":
-                complementoSalario = salarioBase * 0.10;
-                break;
-            case "B":
-                complementoSalario = salarioBase * 0.15;
-                break;
-            case "C":
-                complementoSalario = salarioBase * 0.05;
-                break;
-            case "D":
-                complementoSalario = salarioBase * 0.12;
-                break;
-            default:
-                System.out.println("Departamento no válido");
-                return;
+            switch (departamento) {
+                case "A":
+                    complementoSalario = salarioBase * 0.10;
+                    break;
+                case "B":
+                    complementoSalario = salarioBase * 0.15;
+                    break;
+                case "C":
+                    complementoSalario = salarioBase * 0.05;
+                    break;
+                case "D":
+                    complementoSalario = salarioBase * 0.12;
+                    break;
+                default:
+                    System.out.println("Departamento no válido");
+                    return;
+            }
+
+            double salarioTotal = salarioBase + complementoSalario;
+
+            System.out.println("Departamento: " + NombreDepartamento(departamento));
+            System.out.println("Complemento de Salario: " + complementoSalario);
+            System.out.println("Salario Total para " + nombre + ": " + salarioTotal);
+            System.out.println("-------------------");
+
+            System.out.println("¿Desea hacer un nuevo cálculo? (si/no) ");
+            continuar = scanner.nextLine();
         }
-
-        double salarioTotal = salarioBase + complementoSalario;
-
-        System.out.println("Departamento: " + NombreDepartamento(departamento));
-        System.out.println("Complemento de Salario: " + complementoSalario);
-        System.out.println("Salario Total para " + nombre + ": " + salarioTotal);
     }
 
     public static String NombreDepartamento(String departamento) {
